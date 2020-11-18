bf5e549d41ff72e04be557522aa7d93af731965a
ilarisilander <ilarisilander@hotmail.com>
2020-11-18 15:35:54 +0100

    Rename matglad.robot and Add comments in getNext.py

--- src/robot/scripts/getNext.py
+++ src/robot/scripts/getNext.py
@@ -10,7 +10,7 @@ This script makes a GET request (hasNext) to the graphwalker REST API to see if
 the API has any more steps in the model.
 Then it makes another GET request (getNext) to get the name of the next step in the model.
 
-The name is then returned to the RobotFramework test and the name is also sent to the communicator. 
+The name is then returned to the RobotFramework test and the name is also sent to the communicator.
 '''
 
 def get_next():
@@ -19,8 +19,8 @@ def get_next():
     if response_json['hasNext'] == 'true': # Check if there are any more steps in the REST API
         get_next_response = requests.get('http://localhost:8887/graphwalker/getNext')
         json_file = json.loads(get_next_response.text)
-        #communicator.get_data(json_file['currentElementName']) # Send data to communicator
-        return json_file['currentElementName'] # Send data to RobotFramework
+        #communicator.get_data(json_file['currentElementName']) # Send step name to communicator
+        return json_file['currentElementName'] # Send step name to RobotFramework
     elif response_json['hasNext'] == 'false':
         return 'false'
 
